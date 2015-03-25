cbda15f55e7cf7c73e8422b27d2b0bb6cb731289
Ian Kronquist <iankronquist@teleport.com>
2015-03-24 18:13:54 -0700

    Set up gdt in kernel main and add debugging info

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,11 @@
 #include "vga_driver.h"
+#include "gdt.h"
 
 void kernel_main() {
     vga_initialize();
-    vga_putstr("Hello kernel!");
+    vga_putstr("Hello kernel!\n");
+    vga_putstr("Loading GDT...\n");
+    setup_gdt();
+    vga_putstr("Done!\n");
+    print_gdt();
 }
